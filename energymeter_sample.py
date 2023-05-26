91bf2c3a4447fa2e5fa8613ea2e2f884105057b4
PauloRadatz <pradatz@epri.com>
2023-05-26 11:39:49 -0400

    Reactive power morteza

--- energymeter_sample.py
+++ energymeter_sample.py
@@ -22,20 +22,39 @@ dss.meters.sample()
 register_names = dss.meters.register_names
 register_values = dss.meters.register_values
 
-print(f"Feeder demand - kW")
+print(f"Feeder demand")
 p_f = register_values[register_names.index('kWh')]
+q_f = register_values[register_names.index('kvarh')]
 print(f"p_f={p_f} kW\n")
+print(f"q_f={q_f} kvar\n")
 
-print(f"Feeder Losses - kW")
+print(f"Feeder Losses")
 p_l = register_values[register_names.index('Zone Losses kWh')]
+q_l = register_values[register_names.index('Zone Losses kvarh')]
 print(f"p_l={p_l} kW\n")
+print(f"q_l={q_l} kvar\n")
 
-print(f"Feeder Load Demand - kW")
+print(f"Feeder Load Demand")
 p_load = register_values[register_names.index('Zone kWh')]
+q_load = register_values[register_names.index('Zone kvarh')]
 print(f"p_load={p_load} kW\n")
+print(f"q_load={q_load} kvar\n")
 
-print(f"Feeder Gen or Storage Demand - kW")
+print("Feeder Capacitor")
+q_c = 0
+dss.capacitors.first()
+for _ in range(dss.capacitors.count):
+    q_c = q_c - sum(dss.cktelement.powers[1: dss.cktelement.num_phases * 2: 2])
+    dss.capacitors.next()
+print(f"q_c={q_c} kvar\n")
+
+print(f"Feeder Gen or Storage Demand")
 p_g = p_f - p_l - p_load
+q_g = q_f + q_c - q_l - q_load
 print(f"p_g={p_g} kW\n")
+
+print("Power Balance - should be 0")
+print(f"0 = {p_f + p_g - p_load - p_l}")
+print(f"0 = {q_f + q_g + q_c- q_load - q_l}")
 
 dss.text("show meters")
